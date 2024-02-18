541101ea295fd48643b4ed94f66bb60f0dd0578f
daniel_norena <dnb0512@gmail.com>
2024-02-17 21:33:54 -0500

    Continue working on the day 6 lecture. New folders created in dia6

---
+++ python/dia6/directorios.py
@@ -0,0 +1,40 @@
+import os
+
+
+route=os.getcwd() #getcurrent working directory
+
+if route != '/mnt/e/code/python/dia6/alternativo':
+    os.chdir('/mnt/e/code/python/dia6/alternativo') #chdir allows to change the working directory
+    print(f'current directory changed to {os.getcwd()}')
+
+try: #The os.makedirs() method returns an error if the route already exist
+    os.makedirs('./test') #This method creates a directory in the specified route
+except:
+    pass
+
+route='/mnt/e/code/python/dia6/alternativo/alternativo.txt'
+print(f'The route is {route}')
+elemento=os.path.basename(route)
+directory=os.path.dirname(route)
+elements=os.path.split(route)
+print(f'Working in the {elemento} file located in the {directory} folder. Reference {elements}')
+
+os.mkdir('./otra') #Creates a folder
+print(os.listdir()) #prints the folders
+os.rmdir('./otra') #Removes the folder
+print(os.listdir())
+
+from pathlib import Path #From the pathlib library we import the Path module
+os.chdir('..')
+route=Path(os.getcwd()+'/alternativo') #Object Path allows to create a route from our working folder
+print(route)
+file=route / 'alternativo.txt'
+my_file=open(file)
+print(my_file.read())
+my_file.close()
+
+
+
+
+
+
--- python/dia6/entradasalida.py
+++ python/dia6/entradasalida.py
@@ -21,13 +21,23 @@ print(lines_list)
 #mi_archivo=mi_archivo.write('Soy el nuevo texto') #Raises an error as 'r' doesnt allow writing
 
 mi_archivo=open('prueba.txt','w')
-mi_archivo.write('Soy el nuevo texto') 
-print(mi_archivo.read())
-
-
-
+mi_archivo.write('Soy el nuevo texto\n') #line breaks are not included with .write. If writing multiple lines make sure to include it
+mi_archivo.write('Esta es la segunda linea\n')  
+mi_archivo.writelines(['list','from','write','lines']) #This method takes strings in a list and writes them in a line continuosly
+mi_archivo.writelines(['list\n','from\n','write\n','lines\n']) #line breakers can be added for conviniece
+mi_archivo=open('prueba.txt') #After .write file needs to be reopen for updates
+print(mi_archivo.read()) #line breakers can be added for conviniece
 
 mi_archivo.close() #Use always that you call the open method
+
+mi_archivo=open('prueba.txt','a') #the a parameter allows to add lines to the existing text
+mi_archivo.write('Este texto fue incluido usando el parametro a en el metodo open')
+mi_archivo=open('prueba.txt')
+print(mi_archivo.read())
+mi_archivo.close()
+
+
+
 
 
 
